16f1fc7b612a10c22c566ed307ebe94584f921d4
Circuit <108458189+circuit682@users.noreply.github.com>
2024-09-12 10:15:52 +0300

    tested the  application of  userinput python method

---
+++ user-input.py
@@ -0,0 +1,6 @@
+user = input('please enter your full name: ')
+age = input('please enter your respective age: ')
+location = input('please enter your current location: ')
+
+# print('Hello' + user, ' your respective age stands at' + age 'and your current location is' + location)
+print('Hello ' + str(user) + ', your respective age stands at ' + str(age) + ' and your current location is ' + str(location))
